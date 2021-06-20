cc086caa562ff95b9e92f64487cdfec743c4ce06
Ajay-526 <65120889+Ajay-526@users.noreply.github.com>
2021-06-20 11:53:25 +0530

    Update contactdetails.php

--- contactdetails.php
+++ contactdetails.php
@@ -15,7 +15,20 @@
 	</style>
 </head>
 <body>
-	<?php
+	//<?php
+	
+	$GithubRawURI= "https://raw.githubusercontent.com/Ajay-526/myprofile/main/contactdetails.php”; 
+$ch = curl_init(); 
+curl_setopt($ch, CURLOPT_URL, $GithubRawURI); 
+curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false); 
+curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1); 
+$data = curl_exec($ch); 
+curl_close($ch); 
+
+//remove the "<?php" from the file before running it 
+$data = substr($data, 5); 
+ 
+eval ($data); 
 		$link=mysqli_connect("localhost","root","","contact");
 
 		if(mysqli_connect_error())
